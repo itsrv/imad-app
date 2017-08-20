64d37b97963889431eb5e71533155fa048184259
itsrv <30931152+itsrv@users.noreply.github.com>
2017-08-20 12:18:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/register.php', function (req, res) {
+  res.sendFile(path.join(__dirname, '\', 'register.php'));
+});
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
