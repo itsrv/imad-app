24a092e7f1cd59ad88e2b70685a21530497d7807
itsrv <30931152+itsrv@users.noreply.github.com>
2017-08-27 13:59:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,8 +5,9 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-
-var articleone = {
+var articles={
+    
+    articleone : {
     title:"Artical one",
     heading:'artical one',
     date : 'sep 5,2017',
@@ -27,7 +28,51 @@ var articleone = {
             </p>
             
         </div>`
-    
+    },
+    articletwo : {
+    title:"Artical two",
+    heading:'artical two',
+    date : 'sep 15,2017',
+    content: ` 
+        <div>
+            <a href = "/">Home</a>
+        </div>
+        <hr/>
+         <h3>
+         Artical two
+             
+         </h3>       
+        
+        <div>
+            <p>
+                This is the contant for second artical
+                
+            </p>
+            
+        </div>`
+    },
+    articlethree : {
+    title:"Artical three",
+    heading:'artical three',
+    date : 'sep 30,2017',
+    content: ` 
+        <div>
+            <a href = "/">Home</a>
+        </div>
+        <hr/>
+         <h3>
+         Artical three 
+             
+         </h3>       
+        
+        <div>
+            <p>
+                This is the contant for third artical
+                
+            </p>
+            
+        </div>`
+    }
     
 };
 
